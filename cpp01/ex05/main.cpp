b4c07d72fa0ff09037af302db8cc2c4246b47fe7
idkhl <ines.dakhlaoui@gmail.com>
2025-01-31 18:25:38 +0100

    m01ex04/05 done

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+void complain(std::string level);
+
+int main()
+{
+	Harl harl;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	return 0;
+}
