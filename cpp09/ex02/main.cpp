bc185eaefad6ed73f3123a7428ff1ec4b533b26a
idkhl <ines.dakhlaoui@gmail.com>
2025-04-01 18:40:57 +0200

    poisson

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -4,6 +4,11 @@ int main(int ac, char **av)
 {
 	if (ac == 1)
 	{
-		std::cerr << "Format: ./P";
+		std::cerr << "Format: ./PmergeMe 1 2 3 4 5" << std::endl;
+		return 1;
 	}
+	std::cout << "Before: ";
+	for (int i = 1; i < ac; i++)
+		std::cout << av[i] << " ";
+	std::cout << std::endl;
 }
